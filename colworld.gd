b65452d51acbb7f7dc401d85b546c0944711b427
Luis Fernando <luis.fernando.espinosa.perez@hp.com>
2020-02-02 01:06:33 -0600

    IT WORKS
    IT FUCKING WORKS
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- colworld.gd
+++ colworld.gd
@@ -1,8 +1,5 @@
 extends Node2D
 
-#Physics2DServer.areasetparam(getworld2d.getspace())
-#Physics2DServer.area_param_Gravity(500, Vector2(0, 0))
-
 func _on_princess_body_enter(body):
 	# The name of this editor-generated callback is unfortunate
 	if body.get_name() == "player":
