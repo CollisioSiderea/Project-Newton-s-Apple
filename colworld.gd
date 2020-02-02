e87817584ef0f3d227102f3288d1d49aad36c34f
LBanda <A01610329@itesm.mx>
2020-02-02 04:00:59 -0600

    timer

--- colworld.gd
+++ colworld.gd
@@ -1,10 +1,17 @@
 extends Node2D
 
 func _on_princess_body_enter(body):
-		
+	
 	if body.get_name() == "caja":
 		$pared.move_local_y(50,true)
 		$puente.move_local_x(60,true)
+		var t = Timer.new() 		# Create a new Timer node
+		t.set_wait_time(5.5) 		# Set the wait time
+		add_child(t)			# Add it to the node tree as the direct child
+		t.start()			# Start it
+		yield(t, "timeout")		# Finally, make the script stop with the yield
+		$pared.move_local_y(-50,true)
+		$puente.move_local_x(-60,true)
 		
 	#else:
 		#$pared.move_local_y(0,false)
