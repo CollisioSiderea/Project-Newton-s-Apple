63595a031cb38e6b9de1950906ddbdb008bd9aa5
Luis Fernando <luis.fernando.espinosa.perez@hp.com>
2020-02-02 05:34:02 -0600

    waat

--- colworld.gd
+++ colworld.gd
@@ -1,27 +1,11 @@
 extends Node2D
 
 func _on_princess_body_enter(body):
-	
+		
 	if body.get_name() == "caja":
 		$pared.move_local_y(50,true)
-		#$puente.move_local_x(60,true)
-		var t = Timer.new() 		# Create a new Timer node
-		t.set_wait_time(5.5) 		# Set the wait time
-		add_child(t)			# Add it to the node tree as the direct child
-		t.start()			# Start it
-		yield(t, "timeout")		# Finally, make the script stop with the yield
-		$pared.move_local_y(-50,true)
-		#$puente.move_local_x(-60,true)
-		
-
-
-func _on_bottom_input_event(viewport, event, shape_idx):
-		
 		$puente.move_local_x(60,true)
-		var t = Timer.new() 		# Create a new Timer node
-		t.set_wait_time(5.5) 		# Set the wait time
-		add_child(t)			# Add it to the node tree as the direct child
-		t.start()			# Start it
-		yield(t, "timeout")		# Finally, make the script stop with the yield
-		#$pared.move_local_y(-50,true)
-		$puente.move_local_x(-60,true)
+		
+	#else:
+		#$pared.move_local_y(0,false)
+		#$puente.move_local_x(0,false)
