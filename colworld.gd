71d26fd025574ed894d3ccdc983ca1979c30f46b
LBanda <A01610329@itesm.mx>
2020-02-02 07:31:22 -0600

    salidas

--- colworld.gd
+++ colworld.gd
@@ -1,22 +1,6 @@
 extends Node2D
 var push = Input.is_action_just_pressed("push")
 
-func _on_princess_body_enter(body):
-		
-	if body.get_name() == "caja":
-		$pared.move_local_y(50,true)
-		
-		var t = Timer.new() 		# Create a new Timer node
-		t.set_wait_time(5.5) 		# Set the wait time
-		add_child(t)			# Add it to the node tree as the direct child
-		t.start()			# Start it
-		yield(t, "timeout")		# Finally, make the script stop with the yield
-		
-		$pared.move_local_y(-50,true)
-
-
-
-
 		#$puente.move_local_x(60,true)
 		
 		#var t = Timer.new() 		# Create a new Timer node
@@ -31,3 +15,23 @@ func _on_princess_body_enter(body):
 func _on_muerte_body_entered(body):
 	if body.get_name() == "caja":
 		get_tree().reload_current_scene()
+
+
+
+
+func _on_princess_body_entered(body):
+	if body.get_name() == "caja":
+		$pared.move_local_y(100,true)
+		
+		var t = Timer.new() 		# Create a new Timer node
+		t.set_wait_time(5.5) 		# Set the wait time
+		add_child(t)			# Add it to the node tree as the direct child
+		t.start()			# Start it
+		yield(t, "timeout")		# Finally, make the script stop with the yield
+		
+		$pared.move_local_y(-100,true)
+
+
+func _on_salida_body_entered(body):
+	if body.get_name() == "player":
+		get_tree().change_scene("world2.tscn")
