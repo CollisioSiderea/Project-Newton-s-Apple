ff59ff3272ac9788f86d38656f9edf6ce6177158
LBanda <A01610329@itesm.mx>
2020-02-02 02:00:24 -0600

    a

--- colworld.gd
+++ colworld.gd
@@ -1,17 +1,11 @@
 extends Node2D
 
 func _on_princess_body_enter(body):
-	# The name of this editor-generated callback is unfortunate
+		
 	if body.get_name() == "caja":
-		#$youwin.show()
-		#$pared.rotate(-5000)
-		#$pared. remove_and_skip()
-		#$pared. hide()
-		$caja.remove_and_skip()
 		$pared.move_local_y(50,true)
 		$puente.move_local_x(60,true)
 		
 	else:
-		#$pared. show()
 		$pared.move_local_y(0,false)
 		$puente.move_local_x(0,false)
