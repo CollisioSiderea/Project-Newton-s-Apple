3544352ae310e13b7e4273e6c89ca391752f2c03
LBanda <A01610329@itesm.mx>
2020-02-02 11:02:47 -0600

    triggers

--- colworld.gd
+++ colworld.gd
@@ -1,15 +1,7 @@
 extends Node2D
 var push = Input.is_action_just_pressed("push")
 
-		#$puente.move_local_x(60,true)
 		
-		#var t = Timer.new() 		# Create a new Timer node
-		#t.set_wait_time(5.5) 		# Set the wait time
-		#add_child(t)			# Add it to the node tree as the direct child
-		#t.start()			# Start it
-		#yield(t, "timeout")		# Finally, make the script stop with the yield
-
-		#$puente.move_local_x(-60,true)
 
 
 func _on_muerte_body_entered(body):
@@ -49,3 +41,29 @@ func _on_salida3_body_entered(body):
 func _on_salida4_body_entered(body):
 	if body.get_name() == "player":
 		get_tree().change_scene("world1.tscn")
+
+
+#func _on_bottom_body_entered(body):
+#	if body.get_name() == "player":
+#		$puente.move_local_x(60,true)
+#		
+#		var t = Timer.new() 		# Create a new Timer node
+#		t.set_wait_time(5.5) 		# Set the wait time
+#		add_child(t)			# Add it to the node tree as the direct child
+#		t.start()			# Start it
+#		yield(t, "timeout")		# Finally, make the script stop with the yield
+
+#		$puente.move_local_x(-60,true)
+
+
+func _on_boton_body_entered(body):
+	if body.get_name() == "player":
+		$pared.move_local_y(100,true)
+		
+		var t = Timer.new() 		# Create a new Timer node
+		t.set_wait_time(9.5) 		# Set the wait time
+		add_child(t)			# Add it to the node tree as the direct child
+		t.start()			# Start it
+		yield(t, "timeout")		# Finally, make the script stop with the yield
+		
+		$pared.move_local_y(-100,true)
