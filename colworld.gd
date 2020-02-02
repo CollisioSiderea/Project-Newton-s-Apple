c3d74f657e8418b0ef0c2f3c482113969658ab6b
LBanda <A01610329@itesm.mx>
2020-02-02 03:43:49 -0600

    b

--- colworld.gd
+++ colworld.gd
@@ -6,6 +6,6 @@ func _on_princess_body_enter(body):
 		$pared.move_local_y(50,true)
 		$puente.move_local_x(60,true)
 		
-	else:
-		$pared.move_local_y(0,false)
-		$puente.move_local_x(0,false)
+	#else:
+		#$pared.move_local_y(0,false)
+		#$puente.move_local_x(0,false)
